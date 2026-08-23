8f809a836f8c0d9ac6f9ea445560fae1322c3278
Atanazio01 <marcosd.atanazio@gmail.com>
2026-08-23 12:20:49 -0300

    Separating API routes into a separate folder

--- src/app/patients/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request): Promise<Response> {
-  const headers = new Headers(request.headers);
-
-  console.log(headers);
-
-  return NextResponse.json({ message: "Hello JStack" });
-}
